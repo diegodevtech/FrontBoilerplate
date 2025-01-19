feb7b3fd677ac35a9738974c0a77a60066a73f34
diegodevtech <diegodevtechnology@gmail.com>
2025-01-19 13:31:06 -0400

    config: adds eslint rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,33 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      'no-unused-vars': 'warn',
+      'no-undef': 'error',
+      'quotes': ['warn', 'single'],
+      'semi': ['warn', 'always'],
+      'linebreak-style': ['error', 'unix'],
+      'no-console': 'warn',
+      'indent': ['warn', 2],
+      'no-var': 'error',
+      'no-const-assign': 'error',
+      'no-cond-assign': 'error',
+      'no-self-assign': 'warn',
+      'no-template-curly-in-string': 'error',
+      'arrow-body-style': ['warn', 'as-needed'],
+      'camelcase': 'error',
+      'capitalized-comments': 'warn',
+      'curly': 'error',
+      'default-case': 'error',
+      'default-case-last': 'warn',
+      'dot-notation': 'error',
+      'eqeqeq': 'error',
+      'no-else-return': 'warn',
+      'prefer-const': 'warn',
+      'require-await': 'error',
+    },
+  }
 ];
 
 export default eslintConfig;
