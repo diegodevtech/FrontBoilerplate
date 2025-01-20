6a0ac5753207f8cbfa256089efa929ff00bde3dd
diegodevtech <diegodevtechnology@gmail.com>
2025-01-20 15:11:10 -0400

    feat: adds commit-msg git hook config

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import styles from './page.module.css';
 export default function Home() {
   return (
     <div className={styles.page}>
-      <div>aaaaaasd</div>
+      <div>aaaaaasdawer</div>
       <main className={styles.main}>
         <Image
           className={styles.logo}
